e2aa3e7ad4788f86e39aecff78bac9bcd61fd8a2
Karol <kargol92@gmail.com>
2020-03-22 19:54:28 +0100

    try service worker

--- service-worker-check.js
+++ service-worker-check.js
@@ -1,7 +1,16 @@
-if ('serviceWorker' in navigator) {
+if (isServiceWorkersSupport) {
+    console.log('Will service worker register?');
+    navigator.serviceWorker.register(PATH).then(function () {
+        console.log("Yes it did.");
+    }).catch(function (err) {
+        console.log("No it didn't. This happened: ", err)
+    });
+}
+
+/*if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker-main.js').then(function(Sregistration) {
-    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
   }).catch(function(err) {
     console.log('ServiceWorker registration failed: ', err);
   });
-}
+}*/
